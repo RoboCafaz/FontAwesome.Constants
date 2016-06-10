a4077988aa3bdaa5d19df504fe267cb77359632b
Bobby Cafazzo <bcafazzo@nbi.company>
2016-06-10 17:10:15 -0400

    Updating generation to do proper unicode.

--- FontAwesome.Constants.Test/GenerationTest.cs
+++ FontAwesome.Constants.Test/GenerationTest.cs
@@ -17,38 +17,24 @@ namespace FontAwesome.Constants.Test
         }
 
         [TestMethod]
-        public void FontAwesomeConstants_ShouldAllHaveBeenGenerated()
-        {
-            var dictionary = BuildLocalDictionary();
-            var fields = GetGeneratedData();
-            Assert.AreEqual(dictionary.Count, fields.Length);
-            foreach (var entry in dictionary)
-            {
-                var value = GetGeneratedField(entry.Key, fields);
-                Assert.IsNotNull(value);
-                Assert.AreEqual(entry.Value, value);
-            }
-        }
-
-        [TestMethod]
         public void FontAwesomeConstants_ShouldHaveDefinitionForPhoto()
         {
             var field = GetGeneratedField("PHOTO");
-            Assert.AreEqual(@"\f03e", field);
+            Assert.AreEqual("\uf03e", field);
         }
 
         [TestMethod]
         public void FontAwesomeConstants_ShouldHaveDefinitionForPictureO()
         {
             var field = GetGeneratedField("PICTURE_O");
-            Assert.AreEqual(@"\f03e", field);
+            Assert.AreEqual("\uf03e", field);
         }
 
         [TestMethod]
         public void FontAwesomeConstants_ShouldHaveDefinitionForImage()
         {
             var field = GetGeneratedField("IMAGE");
-            Assert.AreEqual(@"\f03e", field);
+            Assert.AreEqual("\uf03e", field);
         }
 
         private static string GetGeneratedField(string field, FieldInfo[] fields = null)
@@ -67,46 +53,6 @@ namespace FontAwesome.Constants.Test
             Assert.IsNotNull(fields);
             Assert.IsTrue(fields.Length > 0);
             return fields;
-        }
-
-        private static IDictionary<string, string> BuildLocalDictionary()
-        {
-            var fontDictionary = new Dictionary<string, string>();
-
-            var numbers = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
-            var definitionRegex = new Regex("(\\.fa([^{}])+?):before(.+?)}", RegexOptions.IgnoreCase);
-            var textRegex = new Regex("(?<=content:\")(.+?)(?=\")", RegexOptions.IgnoreCase);
-            var headerRegex = new Regex("(?<=\\.fa-)(.+?)(?=:)", RegexOptions.IgnoreCase);
-
-            var path = "css/font-awesome.min.css";
-            using (StreamReader sr = new StreamReader(path))
-            {
-                var line = sr.ReadToEnd();
-                var definition = definitionRegex.Match(line);
-                while (definition.Success)
-                {
-                    var text = textRegex.Match(definition.Value);
-                    if (text.Success)
-                    {
-                        var header = headerRegex.Match(definition.Value);
-                        while (header.Success)
-                        {
-                            var headerText = header.Value.Replace("-", "_").Replace("fa", "").Replace(".", "");
-                            for (var i = 0; i < 10; i++)
-                            {
-                                headerText = headerText.Replace(i + "", numbers[i] + "");
-                            }
-                            headerText = headerText.ToUpper();
-                            fontDictionary.Add(headerText, text.Value);
-                            header = header.NextMatch();
-                        }
-                    }
-                    definition = definition.NextMatch();
-                }
-            }
-            Assert.IsNotNull(fontDictionary);
-            Assert.IsTrue(fontDictionary.Count > 0);
-            return fontDictionary;
         }
     }
 }
